79a1bc8c8a75224cc5c5c6b7f1582ee38d54db57
Katheryne Arancibia <katheryne.arancib.12@sansano.usm.cl>
2019-03-04 17:10:05 -0300

    ultimo commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.find_or_create_by(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+@user= AdminUser.find_by(email: 'admin@example.com')
+if @user.present?
+    @user= AdminUser
+else
+    AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if AdminUser.where(email: 'admin@example.com').blank?
+end
+
+
+
+30.times do 
+    User.create(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+    )
+end
+
+@users = User.all
+
+20.times do
+Company.create(
+    name: Faker::Company.name
+)
+end
+
+@companies = Company.all
+250.times do 
+Claim.create(
+    user: @users.sample,
+    company: @companies.sample,
+    content: Faker::App.author
+    ) 
+    
+end
+
+
+
+
+
